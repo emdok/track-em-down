fbf61d111f7a555b09fac439b73b0894ef5c6e6e
Emily Dokken <dokkenemily@gmail.com>
2022-01-30 11:15:42 -0800

    create GET route for employee table

---
+++ routes/apiRoutes/employeeRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+// Get all Departments
+router.get('/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+module.exports = router;
