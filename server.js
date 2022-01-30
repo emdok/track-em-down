ea5aebd13aafabf2d32067ec9673f613933a7cc0
Emily Dokken <dokkenemily@gmail.com>
2022-01-30 11:02:00 -0800

    set up express router

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// Use apiRoutes
+app.use("/api", apiRoutes);
 
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
+// Default response for any other requests (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+// Start server after DB connection
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Database connect.");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
