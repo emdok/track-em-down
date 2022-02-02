17f4664e8af4fb795b5ef751bd432949c0aa25d2
Emily Dokken <dokkenemily@gmail.com>
2022-02-01 22:01:11 -0800

    update Add Employee and Add Role prompts to include list of departments, roles, and managers to choose from for better user experience

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
   addDepartment,
   addRole,
   addEmployee,
-  updateEmployees
+  updateEmployees,
 } = require("./utils/roleUtility");
 
 // Prompt inquirer
@@ -55,52 +55,64 @@ const responseMethods = {
       ])
       .then((answers) => addDepartment(answers, app));
   },
-  async AddRole() {
-    await inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "New Role title?",
-        },
-        {
-          type: "input",
-          name: "salary",
-          message: "What is the salary?",
-        },
-        {
-          type: "input", // MAKE THIS A LIST AND ADD A QUERY
-          name: "department",
-          message: "Please enter the department.",
-        },
-      ])
-      .then((answers) => addRole(answers, app));
+  AddRole() {
+    db.query(`SELECT name, id AS value FROM department`, (err, departments) => {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "title",
+            message: "New Role title?",
+          },
+          {
+            type: "input",
+            name: "salary",
+            message: "What is the salary?",
+          },
+          {
+            type: "list",
+            name: "department",
+            message: "Please enter the department.",
+            choices: departments,
+          },
+        ])
+        .then((answers) => addRole(answers, app));
+    });
   },
-  async AddEmployee() {
-    await inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "firstName",
-          message: "First name?",
-        },
-        {
-          type: "input",
-          name: "lastName",
-          message: "Last name?",
-        },
-        {
-          type: "input", // MAKE THIS A LIST AND CREATE A QUERY
-          name: "role", 
-          message: "What is their role id?",
-        },
-        {
-          type: "input", // MAKE THIS A LIST AND CREATE A QUERY
-          name: "manager",
-          message: "Who is their manager?",
-        },
-      ])
-      .then((answers) => addEmployee(answers, app));
+  AddEmployee() {
+    db.query(`SELECT title AS name, id AS value FROM role`, (err, roles) => {
+      db.query(
+        `SELECT CONCAT(first_name,' ', last_name) AS name, manager_id AS value FROM employee`,
+        (err, managers) => {
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "firstName",
+                message: "First name?",
+              },
+              {
+                type: "input",
+                name: "lastName",
+                message: "Last name?",
+              },
+              {
+                type: "list",
+                name: "role",
+                message: "What is their role id?",
+                choices: roles,
+              },
+              {
+                type: "list",
+                name: "manager",
+                message: "Who is their manager?",
+                choices: managers,
+              },
+            ])
+            .then((answers) => addEmployee(answers, app));
+        }
+      );
+    });
   },
   UpdateEmployeeRole() {
     db.query(
@@ -117,13 +129,13 @@ const responseMethods = {
                   choices: employees,
                 },
                 {
-                    type: "list",
-                    name: "role",
-                    choices: roles
-                }
+                  type: "list",
+                  name: "role",
+                  choices: roles,
+                },
               ])
               .then((answers) => {
-                  updateEmployees(answers, app);
+                updateEmployees(answers, app);
               });
           }
         );
@@ -133,7 +145,6 @@ const responseMethods = {
 };
 
 // App function to handle prompting a user and directing them to the correct response method
-
 async function app() {
   const { menuChoice } = await userPrompts.promptHomeMenu();
   if (menuChoice === "Quit") return;
